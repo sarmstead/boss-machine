850382bfb4de5ff836ce8e44bfc1d2d0c16c283d
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-09-22 18:02:25 -0400

    Create basic structure of api.js file

--- server/api.js
+++ server/api.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const apiRouter = express.Router();
+const db = require('./db');
 
+// '/api/minions' routes
+apiRouter.param('minions', (req, res, next, id) => {});
 
+// '/api/ideas' routes
+apiRouter.param('ideas', (req, res, next, id) => {});
+
+// '/api/meetings' routes
+apiRouter.param('meetings', (req, res, next, id) => {});
 
 module.exports = apiRouter;
