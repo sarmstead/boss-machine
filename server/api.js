dee7bee925459b8ced8e8f006e27b36d352fb9a8
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-09-24 15:47:16 -0400

    Add /minions get and post routes

--- server/api.js
+++ server/api.js
@@ -3,7 +3,24 @@ const apiRouter = express.Router();
 const db = require('./db');
 
 // '/api/minions' routes
-apiRouter.param('minions', (req, res, next, id) => {});
+// apiRouter.param('minions', (req, res, next, id) => {});
+
+apiRouter.get('/minions', (req, res, next) => {
+    const minionsArr = db.getAllFromDatabase('minions');
+    res.send(minionsArr);
+});
+
+apiRouter.post('/minions', (req, res, next) => {
+    const name = req.body.name;
+    const title = req.body.title;
+    const salary = req.body.salary;
+    if ((name !== undefined) && (title !== undefined) && (salary !== undefined)) {
+        db.addToDatabase('minions', req.body);
+        res.status(201).send(req.body);
+    } else {
+        res.status(400).send();
+    }
+});
 
 // '/api/ideas' routes
 apiRouter.param('ideas', (req, res, next, id) => {});
