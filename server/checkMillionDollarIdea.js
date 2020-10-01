7f7d1625dac120f6a7367af7e0f30a5d02859e17
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-10-01 11:47:03 -0400

    Create checkMillionDollarIdea middleware function

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,11 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
+    const ideaValue = numWeeks * weeklyRevenue;
+    if (ideaValue >= 1000000) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
